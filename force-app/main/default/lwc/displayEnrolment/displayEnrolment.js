225fdf0eeb6fb33679cb320519562dfb96721d69
DannyJaJr <ldanieljajr@gmail.com>
2023-03-16 10:32:56 -0400

    added 3

--- force-app/main/default/lwc/displayEnrolment/displayEnrolment.js
+++ force-app/main/default/lwc/displayEnrolment/displayEnrolment.js
@@ -1,50 +1,103 @@
 import { LightningElement, wire } from 'lwc';
 import { getListUi } from 'lightning/uiListApi';
+import { createRecord } from 'lightning/uiRecordApi';
+//Importing Enrollment fields
 import ENROLLMENT_OBJECT from '@salesforce/schema/Enrollment__c';
+import ENROLLMENT_NAME from '@salesforce/schema/Enrollment__c.Name';
 import CONTACT_FIELD from '@salesforce/schema/Enrollment__c.Contact__c';
 import COURSE_VERSION_FIELD from '@salesforce/schema/Enrollment__c.Course_Version__c';
 import STATUS_FIELD from '@salesforce/schema/Enrollment__c.Status__c';
 import SYNC_STATUS_FIELD from '@salesforce/schema/Enrollment__c.Sync_Status__c';
+//importing contact full name
+import FULL_NAME_FIELD from '@salesforce/schema/Contact.Name';
+//importing course version name
+import COURSE_VERSION_NAME_FIELD from '@salesforce/schema/Course_Version__c.Name';
 
+
+//Enrolemnt columns fields data
 const COLUMNS = [
-    { label: 'Contact', fieldName: CONTACT_FIELD.fieldApiName, type: 'text' },
-    { label: 'Course Version', fieldName: COURSE_VERSION_FIELD.fieldApiName, type: 'text' },
-    { label: 'Status', fieldName: STATUS_FIELD.fieldApiName, type: 'text' },
-    { label: 'Sync Status', fieldName: SYNC_STATUS_FIELD.fieldApiName, type: 'text' },
+    { label: 'Name', fieldName: ENROLLMENT_NAME.fieldApiName, type: 'Auto Number' },
+    { label: 'Contact', fieldName: CONTACT_FIELD.fieldApiName, type: 'Lookup(Contact)' },
+    { label: 'Course Version', fieldName: COURSE_VERSION_FIELD.fieldApiName, type: 'Lookup(Course Version)' },
+    { label: 'Status', fieldName: STATUS_FIELD.fieldApiName, type: 'Picklist' },
+    { label: 'Sync Status', fieldName: SYNC_STATUS_FIELD.fieldApiName, type: 'Picklist' },
     {
         type: 'action',
-        typeAttributes: { rowActions: [{ label: 'Edit', name: 'edit' }] }
+        typeAttributes: { rowActions: [{ label: 'Create', name: 'create' }] }
     }
 ];
 
-export default class DisplayEnrolment extends LightningElement {
+const ENROLLMENT_FIELDS = [
+    ENROLLMENT_NAME,
+    CONTACT_FIELD,
+    COURSE_VERSION_FIELD,
+    STATUS_FIELD,
+    SYNC_STATUS_FIELD
+];
+
+
+
+export default class CreateEnrollment extends LightningElement {
     columns = COLUMNS;
     enrollments = [];
 
     @wire(getListUi, {
         objectApiName: ENROLLMENT_OBJECT,
-        listViewApiName: 'All'
+        listViewApiName: 'All',
+        fields: [...ENROLLMENT_FIELDS, 'Contact__r.' + FULL_NAME_FIELD.fieldApiName, 'Course_Version__r.' + COURSE_VERSION_NAME_FIELD.fieldApiName]
     })
     wiredEnrollments({ error, data }) {
+        console.log("Yes ther is data");
         if (data) {
             this.enrollments = data.records.records.map(record => ({
                 Id: record.id,
-                ...record.fields
+                Name: record.fields.Name ? record.fields.Name.value : '',
+                Contact__c: record.fields.Contact__c ? record.fields.Contact__c.value : '',
+                Contact__r: record.fields.Contact__c ? record.fields.Contact__c.displayValue : '',
+                Course_Version__c: record.fields.Course_Version__c ? record.fields.Course_Version__c.value : '',
+                Course_Version__r: record.fields.Course_Version__c ? record.fields.Course_Version__c.displayValue : '',
+                Status__c: record.fields.Status__c ? record.fields.Status__c.value : '',
+                Sync_Status__c: record.fields.Sync_Status__c ? record.fields.Sync_Status__c.value : ''
             }));
         } else if (error) {
             console.error(error);
         }
     }
 
-    handleRowAction(event) {
-        const actionName = event.detail.action.name;
-        const row = event.detail.row;
-        switch (actionName) {
-            case 'edit':
-                // Handle edit action
-                break;
-            default:
-                break;
-        }
+
+    createNewEnrollment(contactId, courseVersionId) {
+        const fields = {};
+        fields[ENROLLMENT_NAME.fieldApiName] = ''; // Auto-number field will be populated automatically
+        fields[CONTACT_FIELD.fieldApiName] = contactId;
+        fields[COURSE_VERSION_FIELD.fieldApiName] = courseVersionId;
+        fields[STATUS_FIELD.fieldApiName] = 'Pending Enrollment';
+        fields[SYNC_STATUS_FIELD.fieldApiName] = 'Pending Enrollment';
+
+        const recordInput = { apiName: ENROLLMENT_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then((enrollment) => {
+                console.log('New enrollment created with Id: ', enrollment.id);
+                // Add the new enrollment to the list of enrollments displayed in the component
+                this.enrollments = [...this.enrollments, {
+                    Id: enrollment.id,
+                    Name: enrollment.fields[ENROLLMENT_NAME.fieldApiName].value,
+                    Contact__c: enrollment.fields[CONTACT_FIELD.fieldApiName].value,
+                    Course_Version__c: enrollment.fields[COURSE_VERSION_FIELD.fieldApiName].value,
+                    Status__c: enrollment.fields[STATUS_FIELD.fieldApiName].value,
+                    Sync_Status__c: enrollment.fields[SYNC_STATUS_FIELD.fieldApiName].value
+                }];
+            })
+            .catch((error) => {
+                console.error('Error creating enrollment: ', error);
+            });
+
     }
+
+
 }
+
+
+
+
+
+
--- force-app/main/default/lwc/enrollCourses/enrollCourses.js
+++ force-app/main/default/lwc/enrollCourses/enrollCourses.js
@@ -5,28 +5,15 @@ import { getRecord, createRecord } from 'lightning/uiRecordApi';
 //Importing Enrollment fields
 import ENROLLMENT_OBJECT from '@salesforce/schema/Enrollment__c';
 import ENROLLMENT_NAME from '@salesforce/schema/Enrollment__c.Name';
-import CONTACT_FIELD from '@salesforce/schema/Enrollment__c.Contact__c';
-import COURSE_VERSION_FIELD from '@salesforce/schema/Enrollment__c.Course_Version__c';
 import STATUS_FIELD from '@salesforce/schema/Enrollment__c.Status__c';
 import SYNC_STATUS_FIELD from '@salesforce/schema/Enrollment__c.Sync_Status__c';
 //importing contact full name
-import FULL_NAME_FIELD from '@salesforce/schema/Contact.Full_Name__c';
+import FULL_NAME_FIELD from '@salesforce/schema/Contact.Name';
 //importing course version name
 import COURSE_VERSION_NAME_FIELD from '@salesforce/schema/Course_Version__c.Name';
 
 
-//Enrolemnt columns fields data
-const COLUMNS = [
-    { label: 'Name', fieldName: ENROLLMENT_NAME.fieldApiName, type: 'Auto Number' },
-    { label: 'Contact', fieldName: CONTACT_FIELD.fieldApiName, type: 'Lookup(Contact)' },
-    { label: 'Course Version', fieldName: COURSE_VERSION_FIELD.fieldApiName, type: 'Lookup(Course Version)' },
-    { label: 'Status', fieldName: STATUS_FIELD.fieldApiName, type: 'Picklist' },
-    { label: 'Sync Status', fieldName: SYNC_STATUS_FIELD.fieldApiName, type: 'Picklist' },
-    {
-        type: 'action',
-        typeAttributes: { rowActions: [{ label: 'Edit', name: 'edit' }] }
-    }
-];
+
 
 const ENROLLMENT_FIELDS = [
     ENROLLMENT_NAME,
@@ -36,16 +23,9 @@ const ENROLLMENT_FIELDS = [
     SYNC_STATUS_FIELD
 ];
 
-/**Define the JavaScript function named "createNewEnrollment". 
- * This function will use the Lightning Data Service to retrieve the necessary
- *  record data, and then create a new enrollment record using the retrieved
- *  data: 
- **/
 
-//contact : Obi-Wan Kenobi
-//course version : Healthy Habits - Fall 2023
+
 export default class CreateEnrollment extends LightningElement {
-    //Retieve Enrollment data
     columns = COLUMNS;
 
     enrollments = [];
@@ -62,103 +42,58 @@ export default class CreateEnrollment extends LightningElement {
             this.enrollments = data.records.records.map(record =>({
                 Id: record.id,
                 Name: record.fields.Name ? record.fields.Name.value : '',
-                Contact__c: record.fields.Contact__c ? record.fields.Contact__c.value : '',
-                Contact__r: record.fields.Contact__c ? record.fields.Contact__c.displayValue : '',
-                Course_Version__c: record.fields.Course_Version__c ? record.fields.Course_Version__c.value : '',
-                Course_Version__r: record.fields.Course_Version__c ? record.fields.Course_Version__c.displayValue : '',
-                Status__c: record.fields.Status__c ? record.fields.Status__c.value : '',
-                Sync_Status__c: record.fields.Sync_Status__c ? record.fields.Sync_Status__c.value : ''
+                Contact__r: record.fields.Contact__c ? record.fields.Contact__c.value : '',
+
+                Course_Version__r: record.fields.Course_Version__c ? record.fields.Course_Version__c.value : '',
+                 Sync_Status__c: record.fields.Sync_Status__c ? record.fields.Sync_Status__c.value : ''
             }));
         } else if (error) {
             console.error(error);
         }
     }
 
-    handleRowAction(event) {
-        const actionName = event.detail.action.name;
-        const row = event.detail.row;
-        switch (actionName) {
-            case 'edit':
-                // Handle edit action
-                break;
-            default:
-                break;
-        }
-    }
+    /**Define the JavaScript function named "createNewEnrollment". 
+ * This function will use the Lightning Data Service to retrieve the necessary
+ *  record data, and then create a new enrollment record using the retrieved
+ *  data: 
+ **/
 
-
-
-
-
-
-
-
-
-
-    @api recordId;
-
-    @wire(getObjectInfo, { objectApiName: ENROLLMENT_OBJECT })
-    enrollmentObjectInfo;
-
-    @wire(getRecord, { recordId: '$recordId', fields: [CONTACT_FIELD] })
-    contactRecord;
-
-    @wire(getRecord, { recordId: '$recordId', fields: [COURSE_VERSION_FIELD] })
-    courseVersionRecord;
-
-    createNewEnrollment() {
-        const enrollmentRecord = {
-            [CONTACT_FIELD.fieldApiName]: this.contactRecord.data.fields[FULL_NAME_FIELD.fieldApiName].value,
-            [COURSE_VERSION_FIELD.fieldApiName]: this.courseVersionRecord.data.fields[COURSE_VERSION_NAME_FIELD.fieldApiName].value,
-            [STATUS_FIELD.fieldApiName]: 'Pending Enrollment',
-            [SYNC_STATUS_FIELD.fieldApiName]: 'Pending Enrollment'
-        };
-
-        // Use the Lightning Data Service to create a new enrollment record
-
-        createRecord({
-            apiName: ENROLLMENT_OBJECT.objectApiName,
-            fields: enrollmentRecord
-        })
-            .then(() => {
-                // Reset the form
-                this.contactRecord = null;
-                this.courseVersionRecord = null;
-
-                // Show a success message
-                const toastEvent = new ShowToastEvent({
-                    title: 'Success!',
-                    message: 'Enrollment record created.',
-                    variant: 'success'
-                });
-                this.dispatchEvent(toastEvent);
+    
+    
+    createNewEnrollment(contactId, courseVersionId) {
+        const fields = {};
+        fields[ENROLLMENT_NAME.fieldApiName] = ''; // Auto-number field will be populated automatically
+        fields[CONTACT_FIELD.fieldApiName] = contactId;
+        fields[COURSE_VERSION_FIELD.fieldApiName] = courseVersionId;
+        fields[STATUS_FIELD.fieldApiName] = 'Pending Enrollment';
+        fields[SYNC_STATUS_FIELD.fieldApiName] = 'Pending Enrollment';
+    
+        const recordInput = { apiName: ENROLLMENT_OBJECT.objectApiName, fields };
+        createRecord(recordInput)
+            .then((enrollment) => {
+                console.log('New enrollment created with Id: ', enrollment.id);
+                // Add the new enrollment to the list of enrollments displayed in the component
+                this.enrollments = [...this.enrollments, {
+                    Id: enrollment.id,
+                    Name: enrollment.fields[ENROLLMENT_NAME.fieldApiName].value,
+                    Contact__c: enrollment.fields[CONTACT_FIELD.fieldApiName].value,
+                    Course_Version__c: enrollment.fields[COURSE_VERSION_FIELD.fieldApiName].value,
+                    Status__c: enrollment.fields[STATUS_FIELD.fieldApiName].value,
+                    Sync_Status__c: enrollment.fields[SYNC_STATUS_FIELD.fieldApiName].value
+                }];
             })
-            .catch(error => {
-                // Show an error message
-                const toastEvent = new ShowToastEvent({
-                    title: 'Error!',
-                    message: error.body.message,
-                    variant: 'error'
-                });
-                this.dispatchEvent(toastEvent);
+            .catch((error) => {
+                console.error('Error creating enrollment: ', error);
             });
 
-
-
     }
+
+    
+
+
+            
+            
+    
 }
-
-// //new data
-// import { LightningElement, wire } from 'lwc';
-// import { getPicklistValues } from 'lightning/uiObjectInfoApi';
-// import INDUSTRY_FIELD from '@salesforce/schema/Account.Industry';
-
-// export default class Example extends LightningElement {
-//     @wire(getPicklistValues, { recordTypeId: '012000000000000AAA', fieldApiName: INDUSTRY_FIELD })
-//     propertyOrFunction;
-// }
-
-
-
 
 
